9f83e4b2c6ea94dfb5466bf29bd7b354533f19b6
Errorismx <bsry.nitipoom@gmail.com>
2024-01-21 02:25:41 +0700

    Update .config/nvim/lua/custom/mappings.lua Update .config/fish/config.fish

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -4,19 +4,19 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<A-j>"] = {":m .-2 <CR>==", "move down the current line"},
-    ["<A-k>"] = {":m .+1 <CR>==", "move up the current line"},
+    ["<A-k>"] = {":m .-2 <CR>==", "move up the current line"},
+    ["<A-j>"] = {":m .+1 <CR>==", "move down the current line"},
     ["<C-d>"] = {"<C-d>zz"},
     ["<C-u>"] = {"<C-u>zz"},
   },
   i = {
-    ["<A-j>"] = {"<Esc>:m .-2<CR>==gi", "move down the current line"},
-    ["<A-k>"] = {"<Esc>:m .+1<CR>==gi", "move up the current line"}
+    ["<A-k>"] = {"<Esc>:m .-2<CR>==gi", "move up the current line"},
+    ["<A-j>"] = {"<Esc>:m .+1<CR>==gi", "move down the current line"}
   },
   v = {
     [">"] = { ">gv", "indent"},
-    ["<A-j>"] = {":m '>+1<CR>gv=gv", "move down the current line"},
     ["<A-k>"] = {":m '<-2<CR>gv=gv", "move up the current line"}
+    ["<A-j>"] = {":m '>+1<CR>gv=gv", "move down the current line"},
   },
 }
 
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    clear
     neofetch
 end
 alias l='ls -l'
