50ba336acc00a7f559e4c4611f75c11d062a33cb
Errorismx <bsry.nitipoom@gmail.com>
2024-01-20 19:37:00 +0700

    feat: add package list

--- run_once_before_install-packages.sh
+++ run_once_before_install-packages.sh
@@ -1,14 +1,11 @@
 {{ if eq .chezmoi.os "linux" -}}
 #!/bin/sh
-
-sudo apt install ripgrep
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install software-properties-common
+sudo apt install $(cat packages.txt)
 {{ else if eq .chezmoi.os "darwin" -}}
 #!/bin/sh
-brew install \
-fish \
-ripgrep \
-bat \
-lsd \
-zoxide \
-
+brew install $(cat packages.txt)
 {{ end -}}
